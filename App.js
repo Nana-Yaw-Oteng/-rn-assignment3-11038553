3d8d315c9c33c421015c05000a8fd67ddd1b9017
Nana Yaw Owusu Oteng <nyooteng@st.ug.edu.gh>
2024-06-01 23:33:23 +0000

    Updated app with categories

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Greetings from './Componets/Greetings';
 import SearchBox from './Componets/SearchBox';
+import Categories from './Componets/Categories';
 
 export default function App() {
   return (
     <View style={styles.container}>
      <Greetings/>
      <SearchBox/>
+      <Categories/>
       <StatusBar style="auto" />
     </View>
   );
