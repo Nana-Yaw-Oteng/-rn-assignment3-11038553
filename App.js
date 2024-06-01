beefefea6148e9635d72e9c4d40908f59496add2
Nana Yaw Owusu Oteng <nyooteng@st.ug.edu.gh>
2024-06-01 15:58:52 +0000

    completion of search box component and updated App

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Greetings from './Componets/Greetings';
+import SearchBox from './Componets/SearchBox';
 
 export default function App() {
   return (
     <View style={styles.container}>
      <Greetings/>
+     <SearchBox/>
       <StatusBar style="auto" />
     </View>
   );
